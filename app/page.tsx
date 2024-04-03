9f6706e24cde30cc9b4a8a8a68d87b478dee36bf
Pratyush Pandey <Pratyushpandey730@gmail.com>
2024-04-03 15:45:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -194,7 +194,7 @@ export default function Home() {
         </a>
 
              <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -206,7 +206,14 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            I think the task was good to familiarise someone. We can add more scenarios based questions which will help us understand how the candidate thinks based on different scenarios. <br>
+  
+  The candidate can learn tech but one thing which is hard to learn is customer-facing skills, so testing them before hand with some scenarios baked questions will help us understand whether the customer-facing sill is there or not <br>
+  
+  Rest I enjoyed doing this exercise
+  
+  <br>
+  
           </p>
         </a>
         
