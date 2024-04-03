b59208955c24ef04e2416115c3292b9c9797ef05
Pratyush Pandey <Pratyushpandey730@gmail.com>
2024-04-03 15:42:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,48 +3,111 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-      <h2 className={'mb-3 text-2xl font-semibold'}>
-            Question 1<br>{" "}
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              My Five most favourite and five least favourite support tasks are as follow:- 
-  
-              5 Favoutite Tasks 😍 <br>
-  
-              1. Manage a support team - I have a very interesting skill which is the leadership quality, I manage my team like we are building some kind of kingdom. While working in the past I have organised many hackathons with my team and the team management thing is something I love the most. Handling everyone listening to the idea and evolving every day this makes it my top favourite part of managing a team. 
-  
-              2. Scheduling time-off coverage and collaborating as part of a growing cohesive support team - There is a line which says “We grow as a team, and we lose as a team”. No one is working alone or say is the only reason for any good or bad things. We as a team represent any product. Collaboration leads to many ideas and helps us grow as a team and this point is in my favourite list. <br>
-  
-              3. Work with engineering teams during incidents and provide updates to internal and external stakeholders - Everyone wants to know what is really happening, providing them with the updates will make a good impression and more confidence going forward. <br>
-  
-              4. Analyze hundreds of support tickets to spot trends the product team can use - Analyzing is the most important thing in the support, if you get to know that this feature is getting more amount of tickets we can pass the same information to the product team along with the feedback from the client. So that the product team can improve the feature or make it easy to use without the data neither we nor the product team can do anything. Analytics is the king. <br>
-  
-              5. Create video tutorials to help teach users a specific feature or use case - While being in the support role I realised that some topics get the most amount of attention or say the most amount of tickets. Creating a video helping them understand the topic makes their life easier as not all of us want to read the lengthy documentation, video is the simplest way to make anyone understand. <br>
-  
-              <br>
-  
-              5 Least Favourite 😶 <br>
-  
-              1. Act as a dedicated CSE for a handful of key customers to ensure their success using Vercel - This may not sound interesting but I want to be available for most of the issue rather than focusing only on one. My moto is to provide the best client experience so that they can rely on our product in future <br></br>
-  
-              2. Respond to 50+ support requests via email every day - I want to respond to as many tickets as I want to but I don’t want to bind myself with the number. Again my simple motto is to help the client make their life easier <br></br>
-  
-              3. Identify, file (and, where possible, resolve) bugs in private and public Vercel/Next.js repos on GitHub - This is on my list not because I don’t like it but because this is something I like less. I want to and will resolve the bugs in the repos <br></br>
-  
-              4. Engage multiple users at once in a public discussion, to answer their questions and troubleshoot problems - I want to understand the pain of each customer and help them with the solutions, maybe in the public discussion they can’t open up and keep something to themselves. While being in touch with the client separately will help me understand him more closely and they can share their pain points without any worries <br></br>
-  
-              5. Help resolve billing issues for customers - As a support team member I want to understand their pain point help them with all the issues they are facing but billing is something I least like and I always want to redirect them to the billing department for resolutions rather than doing it myself.<br></br>
-  
-  
-                These are my honest opinion based on my preferences. ✌️
-  
-                <br></br><br></br>
-            </p>
-</div>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
   );
 }
